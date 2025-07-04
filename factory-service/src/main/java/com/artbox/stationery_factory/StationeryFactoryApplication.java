4c84c5d4db2b93f62f7a5ef4b77062ecdc0744fe
kornvipa1003 <kornvipa1003@gmail.com>
2025-07-04 23:50:05 +0700

    Add files via upload

---
+++ factory-service/src/main/java/com/artbox/stationery_factory/StationeryFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.artbox.stationery_factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationeryFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationeryFactoryApplication.class, args);
+	}
+
+}
---
+++ factory-service/src/test/java/com/artbox/stationery_factory/StationeryFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artbox.stationery_factory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationeryFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
